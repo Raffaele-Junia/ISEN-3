b0f12d43042d5444f04cbc85d3e6db56f1917b9e
Raffy59 <raffaele.farvacque@outlook.fr>
2026-09-11 00:27:05 +0200

    Delete exo1_pillow.py

--- exo1_pillow.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-im = Image.open("junia.png")
-
-print(im.format, im.size, im.mode)
-box = (900, 200, 1490, 494)
-region = im.crop(box)
-
-region.show()
-region.save("junia-crop.png")
